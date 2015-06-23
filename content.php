8274ef4b8d65a7920a159b08bc88789e5848ef09
Gunnar Hellekson <gunnar@hellekson.com>
2015-06-23 23:06:33 +0000

    use round image size on front page

--- content.php
+++ content.php
@@ -7,8 +7,8 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 		<?php if ( has_post_thumbnail() && ! is_search() ): ?>
-		<div class="post-thumbnail" style="background: url('<?php $thumb_url = wp_get_attachment_image_src( get_post_thumbnail_id(), 'thumbnail'); echo $thumb_url[0]; ?>') no-repeat;">
-			<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(); ?></a>
+		<div class="post-thumbnail" style="background: url('<?php $thumb_url = wp_get_attachment_image_src( get_post_thumbnail_id(), 'round' ); echo $thumb_url[0]; ?>') no-repeat;">
+			<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('circle'); ?></a>
 		</div>
 		<?php endif; ?>
 		<h1 class="entry-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
